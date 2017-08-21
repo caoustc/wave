34ad97296dc966044180040f15cbc0972eaa7dcd
caoustc <caoustc@163.com>
2017-08-21 10:58:21 +0800

    添加效果图

--- app/src/main/java/com/caoustc/test/wave/MainActivity.java
+++ app/src/main/java/com/caoustc/test/wave/MainActivity.java
@@ -1,7 +1,11 @@
 package com.caoustc.test.wave;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AccelerateInterpolator;
+
+import com.caoustc.wave.WaveViewButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WaveViewButton waveViewButton = (WaveViewButton) findViewById(R.id.wave);
+        waveViewButton.setDuration(2000);
+        waveViewButton.setStyle(Paint.Style.STROKE);
+        waveViewButton.setSpeed(1000);
+        waveViewButton.setInterpolator(new AccelerateInterpolator());
+        waveViewButton.start();
     }
 }
